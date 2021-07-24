fe5baf8a19519f099929fef10a500fa25d1213fb
HunterStl09 <felipe.passos.09@gmail.com>
2021-07-24 10:48:19 -0300

    ..

--- Exercícios/Ex#85.py
+++ Exercícios/Ex#85.py
@@ -4,4 +4,23 @@
 Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e impares. No final mostre os valores pares e ímpares em ordem crescente.
 
 '''
+coleção = [[],[]]
 
+for i in range(0,7):
+    i = (int(input('Informe um número: \n')))
+    if i % 2 == 0:
+        coleção[0].append(i)
+    else:
+        coleção[1].append(i)
+
+coleção[0].sort()
+coleção[1].sort()
+
+print(f'Os números pares são:', end=' ')
+for i in coleção[0]:
+    print(i, end=' ')
+print()
+print(f'Os números impares são:', end=' ')
+for i in coleção[1]:
+    print(i, end=' ')
+print()
