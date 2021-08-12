74bc59fd33fa0e9beb10e81985f3931a7868c9f7
HunterStl09 <felipe.passos.09@gmail.com>
2021-08-12 20:56:30 -0300

    ..

--- Exercícios/Ex#94.py
+++ Exercícios/Ex#94.py
@@ -6,3 +6,36 @@ Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os da
 * B) A média de idade do grupo
 * C) Uma lista com todas as mulheres.
 * D) Uma lista com todas as pessoas com idade acima da média.'''
+
+
+people = []
+md = 0
+
+while True:
+    person = {'nome': str(input('Nome pessoa:\n')).strip().upper(),
+              'sexo': str(input('Sexo pessoa:\n')).strip().upper(),
+              'idade': int(input('Idade pessoa:\n'))}
+    people.append(person.copy())
+    
+    md += person['idade']
+    média = md / len(people)
+    
+    cont = str(input('Quer continuar? [ S/N ]')).strip().upper()
+    
+    if cont == 'N':
+        break
+
+print(f'A) Foram cadastradas {len(people)} pessoas.')
+print(f'B) A média de idade do grupo foi {média} anos.')
+print(f'C) as mulheres do grupo foram: ')
+for i in people:
+    if i['sexo'] == 'F':
+        for k,v in i.items():
+            if k == 'nome':
+                print(f'{k}: {v}')
+print(f'D) Pessoas com idade maior que a média:')
+for i in people:
+    if i['idade'] > média:
+        for k, v in i.items():
+            if k == 'nome':
+                print(f'{k}: {v}')
--- Exercícios/Ex#95.py
+++ Exercícios/Ex#95.py
@@ -1,3 +1,45 @@
 '''#Ex#95
 
 Aprimore o exercício 93 para que ele funcione com vários jogadores. Incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.'''
+
+
+jogadores = []
+
+while True:
+    camp = {
+        'nome': str(input('Nome do Jogador: ')).capitalize(),
+        'partidas': [],
+        'total': 0
+    }
+
+    quant = int(input(f'Quantas partidas {camp["nome"]} jogou?'))
+
+    for n,i in enumerate(range(0,quant)):
+        gols = int(input(f'Quantos gols {camp["nome"]} fez na partida {n+1}? '))
+        camp['partidas'].append(gols)
+        camp['total'] += gols
+    
+    jogadores.append(camp.copy())
+    
+    cont = str(input('Quer continuar? [ S/N ]')).strip().upper()
+    
+    if cont == 'N':
+        break
+
+
+def faixa():
+    print('-='*30)
+
+
+faixa()
+print(jogadores)
+
+faixa()
+for k, v in camp.items():
+    print(f'O campo {k} tem o valor {v}.')
+    
+faixa()
+print(f'O jogador {camp["nome"]} jogou {quant} partidas.')
+for n, item in enumerate(camp['partidas']):
+    print(f'     => Na partida {n+1}, fez {item} gols.')
+print(f'Foi um total de {camp["total"]} gols.')
