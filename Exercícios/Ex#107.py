1ae9fb1d37cda1f9c5e3fc1a85b65a0807d6881d
Felipe Passos <felipe.passos.09@gmail.com>
2021-09-14 00:17:51 -0300

    Create Packages and modules on Python

--- Exercícios/Ex#107.py
+++ Exercícios/Ex#107.py
@@ -5,3 +5,9 @@ Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(),
 Faça também um programa que importe esse módulo e use almas dessas funções.
 '''
 
+
+from modules.moeda import *
+
+val = 12.5
+
+print(metade(val))
---
+++ Exercícios/modules/moeda.py
@@ -0,0 +1,33 @@
+def aumentar(arg, aug):
+    """Function destinates to calc the augment of ammount the value input in it.
+
+    Args:
+        arg (float): the value that wants to calculate.
+        aug (int): ammount that needs to be augment on value.
+    """
+    tot = arg + aug
+    
+    return tot
+
+
+def diminuir(arg, sub):
+    """function destiantes to subtract the value.
+
+    Args:
+        arg (float): the value that wants to calculate.
+        sub (int): the value that needs to be subtract on value.
+    """
+    
+    tot = arg - sub
+    
+    return tot
+
+def metade(arg):
+    """function destinates to calc the half of value.
+
+    Args:
+        arg (float): the value that wants to calculate.
+    """
+    tot = arg / 2
+    
+    return tot
